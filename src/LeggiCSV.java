481a3293c055d76443bf95fd3cb597c1c1167989
PANSERI.21255 <panseri.tommaso.studente@itispaleocapa.it>
2025-03-24 13:15:13 +0100

    commit 3

--- src/LeggiCSV.java
+++ src/LeggiCSV.java
@@ -1,32 +1,48 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.*;
 
 public class LeggiCSV {
     public static void main(String[] args) {
-        String nomeFile = "panseri.csv";
+
 
         try {
-            // in questo modo si apre il file
+
+            File nomeFile = new File("panseri.csv");
+            File mioVal = new File("mioValore.cvs");
             BufferedReader br = new BufferedReader(new FileReader(nomeFile));
-            String linea;
+            BufferedWriter bw = new BufferedWriter(new FileWriter("mioValore.csv"));
+            String next;
+            int campi;
+            int num = (int) Math.floor(Math.random()*20);
+            int cont = 0;
 
-            // così leggiamo la prima riga, ovvero l'intestazione
-            if ((linea = br.readLine()) != null) {
-                System.out.println("Intestazione: " + linea);
-            }
+            do{
 
-            // così leggtiamo e stampiamo le prima 5 righe
-            int contatore = 0;
-            while ((linea = br.readLine()) != null && contatore < 5) {
-                System.out.println("Record: " + linea);
-                contatore++;
-            }
+                //PRIMA RICHIESTA
+                next = br.readLine();
+                if(cont == 0){
+                    cont++;
+                    next+=";MioValore";
+                } else {
+                    next+=";" + num;
+                }
+                bw.write(next);
+
+                //SECONDA RICHIESTA
+                campi = next.split(";").length;
+
+                //TERZA RICHIESTA (NON AVANZATO)
+                campi = next.length();
+
+                //QUARTA RICHIESTA
+
+
+            }while(next != null);
 
             // in questo modo chiudiamo il file
             br.close();
         } catch (IOException e) {
-            System.out.println("Errore nella lettura del file!!!!!!!!");
+            System.out.println("Errore nella lettura del file!!!!!!");
             e.printStackTrace();
         }
     }
